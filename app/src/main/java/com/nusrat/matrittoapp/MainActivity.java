095e4e8e2a17ae70b9fedf5e76458ed045036395
Nusrat-Jahan-Nira <89475219+Nusrat-Jahan-Nira@users.noreply.github.com>
2022-03-02 01:34:29 +0600

    version 1.0 [Two button added]

--- app/src/main/java/com/nusrat/matrittoapp/MainActivity.java
+++ app/src/main/java/com/nusrat/matrittoapp/MainActivity.java
@@ -2,17 +2,28 @@ package com.nusrat.matrittoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.nusrat.matrittoapp.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+       // binding.text.setText("Yo");
+
+        binding.datePredictButton.setOnClickListener(v -> {
+
+        });
 
     }
 }
